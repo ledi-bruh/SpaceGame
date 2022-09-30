adabcefa5e8ef8205200ca4786f636bcdf6d1cbd
andreas <podovinov2003andrey@gmail.com>
2022-10-01 00:04:48 +0600

    add mock-tests, Vector equality

--- SpaceGame.Lib.Test/UnitTest1.cs
+++ SpaceGame.Lib.Test/UnitTest1.cs
@@ -1,10 +1,74 @@
 namespace SpaceGame.Lib.Test;
+using Moq;
+using Vector;
 
 public class UnitTest1
 {
+    //* Для объекта, находящегося в точке (12, 5) и движущегося со скоростью (-7, 3) движение меняет положение объекта на (5, 8)
     [Fact]
-    public void Test1()
+    public void TestMoving()
     {
+        Mock<IMovable> mock = new Mock<IMovable>();
+        mock.Setup(x => x.Position).Returns(new Vector(12, 5)).Verifiable();
+        mock.Setup(x => x.Velocity).Returns(new Vector(-7, 3)).Verifiable();
 
+        MoveCommand moveCommand = new MoveCommand(mock.Object);
+        moveCommand.Execute();
+
+        var expected = new Vector(5, 8);
+        mock.VerifySet(x => x.Position = expected, Times.Once);
+    }
+
+    //* Попытка сдвинуть объект, у которого невозможно прочитать положение объекта в пространстве, приводит к ошибке
+    [Fact]
+    public void CantReadPositionInMoving()
+    {
+        Mock<IMovable> mock = new Mock<IMovable>();
+        mock
+        .Setup(x => x.Position)
+        .Throws(new ArgumentException("Can't read object.Position"))
+        .Verifiable();
+
+        MoveCommand moveCommand = new MoveCommand(mock.Object);
+
+        var expected = "Can't read object.Position";
+        var error = Assert.Throws<ArgumentException>(() => moveCommand.Execute());
+        Assert.Equal(error.Message, expected);
+    }
+
+    //* Попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке
+    [Fact]
+    public void CantReadVelocityInMoving()
+    {
+        Mock<IMovable> mock = new Mock<IMovable>();
+        mock
+        .Setup(x => x.Velocity)
+        .Throws(new ArgumentException("Can't read object.Velocity"))
+        .Verifiable();
+
+        MoveCommand moveCommand = new MoveCommand(mock.Object);
+
+        var expected = "Can't read object.Velocity";
+        var error = Assert.Throws<ArgumentException>(() => moveCommand.Execute());
+        Assert.Equal(error.Message, expected);
+    }
+
+    //* Попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке
+    [Fact]
+    public void CantChangePositionInMoving()
+    {
+        Mock<IMovable> mock = new Mock<IMovable>();
+        mock.SetupGet(x => x.Position).Returns(new Vector()).Verifiable();
+        mock.SetupGet(x => x.Velocity).Returns(new Vector()).Verifiable();
+        mock
+        .SetupSet(x => x.Position = It.IsAny<Vector>())
+        .Throws(new ArithmeticException("Can't change object.Position"))
+        .Verifiable();
+
+        MoveCommand moveCommand = new MoveCommand(mock.Object);
+
+        var expected = "Can't change object.Position";
+        var error = Assert.Throws<ArithmeticException>(() => moveCommand.Execute());
+        Assert.Equal(error.Message, expected);
     }
 }
--- SpaceGame.Lib/Vector.cs
+++ SpaceGame.Lib/Vector.cs
@@ -6,6 +6,27 @@ public class Vector
     public int Size => this._elements.Length;
     public Vector(params int[] elements) => this._elements = elements;
     public override string ToString() => $"Vector({string.Join(", ", this._elements)})";
+
+    public static bool operator ==(Vector A, Vector B)
+    {
+        if (A.Size != B.Size) return false;
+        for (int i = 0; i < A.Size; i++) if (A._elements[i] != B._elements[i]) return false;
+        return true;
+    }
+    public static bool operator !=(Vector A, Vector B) => !(A == B);
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(obj, null) || this.GetType() != obj.GetType()) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj is Vector vector &&
+               Size == vector.Size &&
+               _elements.SequenceEqual(vector._elements);
+    }
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
     public static Vector operator +(Vector A, Vector B)
     {
         if (A.Size != B.Size) throw new System.ArgumentException("Dimensions do not match");
