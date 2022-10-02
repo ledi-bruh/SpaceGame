e192a1579653ccbfdf10beb483241fa74f8e0a63
andreas <podovinov2003andrey@gmail.com>
2022-10-02 14:01:41 +0600

    add rotate tests, Angle equality

--- SpaceGame.Lib.Test/UnitTest1.cs
+++ SpaceGame.Lib.Test/UnitTest1.cs
@@ -1,10 +1,84 @@
 namespace SpaceGame.Lib.Test;
+using Moq;
+using Angle;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    //* Корабль, который находится под углом к горизонту в 45 градусов имеет угловую скорость 90 градусов.
+    //* В результате поворота корабль оказывается под углом 135 градусов к горизонту.
+    [Theory]
+    [MemberData(nameof(TestRotatingData))]
+    public void TestRotating(Angle direction, Angle angularVelocity, Angle expected)
     {
+        Mock<IRotatable> mock = new Mock<IRotatable>();
+        mock.Setup(x => x.Direction).Returns(direction).Verifiable();
+        mock.Setup(x => x.AngularVelocity).Returns(angularVelocity).Verifiable();
 
+        RotateCommand rotateCommand = new RotateCommand(mock.Object);
+        rotateCommand.Execute();
+
+        mock.VerifySet(x => x.Direction = expected, Times.Once);
+    }
+    public static TheoryData<Angle, Angle, Angle> TestRotatingData => new()
+    {
+        {new Angle(1, 1), new Angle(0, 1), new Angle(-1, 1)},
+        {new Angle(0, 1), new Angle(0, 1), new Angle(-1, 0)},
+        {new Angle(1, 0), new Angle(1, 0), new Angle(1, 0)},
+        {new Angle(0, 0), new Angle(0, 0), new Angle(0, 0)},
+        {new Angle(0, 0), new Angle(1, 1), new Angle(1, 1)},
+        {new Angle(1, 2), new Angle(0, 0), new Angle(1, 2)},
+    };
+
+    //* Попытка сдвинуть корабль, у которого невозможно прочитать значение угла наклона к горизонту, приводит к ошибке.
+    [Fact]
+    public void CantReadDirectionInRotating()
+    {
+        Mock<IRotatable> mock = new Mock<IRotatable>();
+        mock
+        .Setup(x => x.Direction)
+        .Throws(new ArgumentException("Can't read object.Direction"))
+        .Verifiable();
+
+        RotateCommand rotateCommand = new RotateCommand(mock.Object);
+
+        var expected = "Can't read object.Direction";
+        var error = Assert.Throws<ArgumentException>(() => rotateCommand.Execute());
+        Assert.Equal(error.Message, expected);
+    }
+
+    //* Попытка сдвинуть корабль, у которого невозможно прочитать значение угловой скорости, приводит к ошибке.
+    [Fact]
+    public void CantReadAngularVelocityInRotating()
+    {
+        Mock<IRotatable> mock = new Mock<IRotatable>();
+        mock
+        .Setup(x => x.AngularVelocity)
+        .Throws(new ArgumentException("Can't read object.AngularVelocity"))
+        .Verifiable();
+
+        RotateCommand rotateCommand = new RotateCommand(mock.Object);
+
+        var expected = "Can't read object.AngularVelocity";
+        var error = Assert.Throws<ArgumentException>(() => rotateCommand.Execute());
+        Assert.Equal(error.Message, expected);
+    }
+
+    //* Попытка сдвинуть корабль, у которого изменить угол наклона к горизонту, приводит к ошибке.
+    [Fact]
+    public void CantChangeDirectionInRotating()
+    {
+        Mock<IRotatable> mock = new Mock<IRotatable>();
+        mock.SetupGet(x => x.Direction).Returns(new Angle(0, 0)).Verifiable();
+        mock.SetupGet(x => x.AngularVelocity).Returns(new Angle(0, 0)).Verifiable();
+        mock
+        .SetupSet(x => x.Direction = It.IsAny<Angle>())
+        .Throws(new ArithmeticException("Can't change object.Direction"))
+        .Verifiable();
+
+        RotateCommand rotateCommand = new RotateCommand(mock.Object);
+
+        var expected = "Can't change object.Direction";
+        var error = Assert.Throws<ArithmeticException>(() => rotateCommand.Execute());
+        Assert.Equal(error.Message, expected);
     }
 }
--- SpaceGame.Lib/Angle.cs
+++ SpaceGame.Lib/Angle.cs
@@ -2,17 +2,15 @@ namespace Angle;
 
 public class Angle
 {
-    private readonly int y, x;  //!    y/x => arctan(y/x)
-    private int part; //!    part * pi/2
+    private int x, y, part;  //!    arctan(y/x) + part*pi/2    [-pi/2, 3pi/2]
 
-    public Angle(int y, int x)
+    public Angle(int x, int y)
     {
-        this.y = 0;
-        this.x = 1;
-        if (x == 0) this.part = y > 0 ? 1 : 3;
+        this.x = 1; this.y = 0;
+        if (x == 0 && y == 0) this.part = 0;
+        else if (x == 0) this.part = y > 0 ? 1 : 3;
         else if (y == 0) this.part = x > 0 ? 0 : 2;
         else if (y > 0 && x > 0) { this.y = y; this.x = x; this.part = 0; }
-        else if (y < 0 && x > 0) { this.y = y; this.x = x; this.part = 4; }
         else { this.y = y; this.x = x; this.part = 2; }
     }
     private static int GCD(int a, int b)
@@ -20,15 +18,43 @@ public class Angle
         if (a == 0) return b;
         return GCD(b % a, a);
     }
-
-    public override string ToString() => $"{y}/{x} + {part} * PI/2";
+    private static Angle ZeroForm(Angle Obj)
+    {
+        if (Obj.x == 1 && Obj.y == 0) return Obj;
+        Angle Buf = new Angle(Obj.x, Obj.y);
+        Buf.part = Obj.part;
+        while (Buf.part > 0)
+        {
+            Buf.part -= 1;
+            (Buf.x, Buf.y) = (Buf.y, -Buf.x);
+        }
+        return Buf;
+    }
+    public static bool operator ==(Angle a, Angle b)
+    {
+        Angle A = ZeroForm(a), B = ZeroForm(b);
+        return A.y == B.y && A.x == B.x && A.part == B.part;
+    }
+    public static bool operator !=(Angle a, Angle b) => !(a == b);
+    public override bool Equals(object? obj)
+    {
+        return obj is Angle angle &&
+               this == angle;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y, part);
+    }
+    public override string ToString() => $"Angle({x}, {y}): arctan({y}/{x}) + {part}*PI/2";
     public static Angle operator +(Angle A, Angle B)
     {
-        int y = A.y * B.x + B.y * A.x;
         int x = A.x * B.x - A.y * B.y;
+        int y = A.y * B.x + B.y * A.x;
         int gcd = GCD(y, x);
-        Angle Buf = new Angle(y / gcd, x / gcd);
-        Buf.part = (A.part + B.part + Buf.part) % 8;
+        Angle Buf = new Angle(x / gcd, y / gcd);
+        if (x == B.x && y == B.y) Buf.part += A.part;
+        if (x == A.x && y == A.y) Buf.part += B.part;
+        if (Buf.part > 3) Buf.part %= 4;
         return Buf;
     }
 }
