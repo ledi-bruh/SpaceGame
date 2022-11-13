8a04fa626d2dbc7286457357620fd48b5e93042a
andreas <podovinov2003andrey@gmail.com>
2022-11-13 20:55:04 +0600

    Moved some files

--- SpaceGame.Lib/Commands/Move/MoveCommand.cs
+++ SpaceGame.Lib/Commands/Move/MoveCommand.cs
@@ -3,9 +3,8 @@ namespace SpaceGame.Lib;
 public class MoveCommand : ICommand
 {
     private readonly IMovable _movable;
-    public MoveCommand(IMovable obj)
-    {
-        this._movable = obj;
-    }
+
+    public MoveCommand(IMovable obj) => _movable = obj;
+
     public void Execute() => this._movable.Position += this._movable.Velocity;
 }
