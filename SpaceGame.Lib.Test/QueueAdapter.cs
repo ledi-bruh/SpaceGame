0603ccfb25fe1eff19e3c327153f86c5f5a13ae9
andreas <podovinov2003andrey@gmail.com>
2022-11-22 19:50:24 +0600

    add QueueAdapter for tests

---
+++ SpaceGame.Lib.Test/QueueAdapter.cs
@@ -0,0 +1,12 @@
+namespace SpaceGame.Lib;
+
+public class QueueAdapter<T> : IQueue<T>
+{
+    private Queue<T> _queue;
+
+    public QueueAdapter(Queue<T> queue) => _queue = queue;
+
+    public void Push(T t) => _queue.Enqueue(t);
+
+    public T Pop() => _queue.Dequeue();
+}
