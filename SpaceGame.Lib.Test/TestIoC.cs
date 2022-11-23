ca761a8b446a9e9528bd9caa873b0e859dc86b0f
andreas <podovinov2003andrey@gmail.com>
2022-11-23 20:38:09 +0600

    add Test CreateAdapter

--- SpaceGame.Lib.Test/TestIoC.cs
+++ SpaceGame.Lib.Test/TestIoC.cs
@@ -9,6 +9,18 @@ internal class ThrowResolveDependencyExceptionStrategy : IStrategy
 public class TestIoC
 {
     [Fact]
+    public void TestCreateAdapter()
+    {
+        IoC.Resolve<ICommand>("IoC.Register", "Game.Adapter", new AdapterStrategy()).Execute();
+        IoC.Resolve<ICommand>("IoC.Register", "Game.CreateUObject", new CreateNewObjectStrategy()).Execute();
+
+        IUObject uobj = IoC.Resolve<IUObject>("Game.CreateUObject");
+        IMovable adapter = IoC.Resolve<IMovable>("Game.Adapter", uobj, typeof(MovableAdapter));
+
+        Assert.Equal(typeof(MovableAdapter), adapter.GetType());
+    }
+
+    [Fact]
     public void IoCThrowsResolveDependencyException()
     {
         IoC.Resolve<ICommand>("IoC.Register", "ResolveDependencyException", new ThrowResolveDependencyExceptionStrategy()).Execute();
