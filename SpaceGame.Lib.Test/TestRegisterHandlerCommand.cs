c7fa0dd3f4e63cb7a049ad4ad2fd91adf183cbed
andreas <podovinov2003andrey@gmail.com>
2022-12-22 23:00:00 +0600

    fix RegisterHandlerCommand with tests

--- SpaceGame.Lib.Test/TestRegisterHandlerCommand.cs
+++ SpaceGame.Lib.Test/TestRegisterHandlerCommand.cs
@@ -24,44 +24,57 @@ public class TestRegisterHandlerCommand
     {
         var mockHandler = new Mock<IHandler>();
 
-        var mockExceptionLevelDict = new Mock<IDictionary<object, IHandler>>();
-        mockExceptionLevelDict.Setup(x => x.Add(typeof(ArgumentException), It.IsAny<IHandler>())).Verifiable();
+        var mockExceptionHandleTree = new Mock<IDictionary<int, IHandler>>();
+        mockExceptionHandleTree.Setup(x => x.Add(It.IsAny<int>(), It.IsAny<IHandler>())).Verifiable();
 
-        var mockCommandLevelDict = new Mock<IDictionary<object, IDictionary<object, IHandler>>>();
-        mockCommandLevelDict.Setup(x => x.Add(typeof(CommandTest), It.IsAny<IDictionary<object, IHandler>>())).Verifiable();
-        mockCommandLevelDict.Setup(x => x[typeof(CommandTest)]).Returns(mockExceptionLevelDict.Object).Verifiable();
-
-        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handle.Register",
-            (object[] args) => new RegisterHandlerCommand(args[0], args[1], (IHandler)args[2])
+        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handler.Register",
+            (object[] args) => new RegisterHandlerCommand((IEnumerable<Type>)args[0], (IHandler)args[1])
         ).Execute();
-        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handle.Tree", (object[] args) => mockCommandLevelDict.Object).Execute();
+        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handler.Tree", (object[] args) => mockExceptionHandleTree.Object).Execute();
 
-        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handle.Register", typeof(CommandTest), typeof(ArgumentException), mockHandler.Object).Execute();
-        
-        mockExceptionLevelDict.VerifyAll();
-        mockCommandLevelDict.VerifyAll();
+        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handler.Register", new Type[] { typeof(CommandTest), typeof(ArgumentException) }, mockHandler.Object).Execute();
+
+        mockExceptionHandleTree.VerifyAll();
     }
 
     [Fact]
     public void SuccesfullRegisterSomeExceptionHandlers()
     {
         var mockHandler = new Mock<IHandler>();
-        var dict = new Dictionary<object, IDictionary<object, IHandler>>();
+        var dict = new Dictionary<int, IHandler>();
 
-        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handle.Register",
-            (object[] args) => new RegisterHandlerCommand(args[0], args[1], (IHandler)args[2])
+        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handler.Register",
+            (object[] args) => new RegisterHandlerCommand((IEnumerable<Type>)args[0], (IHandler)args[1])
         ).Execute();
-        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handle.Tree", (object[] args) => dict).Execute();
+        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handler.Tree", (object[] args) => dict).Execute();
 
-        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handle.Register", typeof(CommandTest), typeof(ArgumentException), mockHandler.Object).Execute();
-        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handle.Register", typeof(CommandTest), typeof(ArithmeticException), mockHandler.Object).Execute();
-        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handle.Register", typeof(CommandTest), "Exception.Handle.Exception.Default", mockHandler.Object).Execute();
-        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handle.Register", "Exception.Handle.Command.Default", typeof(ArgumentException), mockHandler.Object).Execute();
-        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handle.Register", "Exception.Handle.Command.Default", typeof(FormatException), mockHandler.Object).Execute();
-        var tree = IoC.Resolve<IDictionary<object, IDictionary<object, IHandler>>>("Exception.Handle.Tree");
+        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handler.Register", new Type[] { typeof(CommandTest), typeof(IOException) }, mockHandler.Object).Execute();
+        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handler.Register", new List<Type> { typeof(ArgumentException), typeof(CommandTest) }, mockHandler.Object).Execute();
+        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handler.Register", new List<Type> { typeof(CommandTest) }, mockHandler.Object).Execute();
+        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handler.Register", new Type[] { typeof(ArgumentException) }, mockHandler.Object).Execute();
+        var tree = IoC.Resolve<IDictionary<int, IHandler>>("Exception.Handler.Tree");
 
-        Assert.Equal(2, tree.Count());
-        Assert.Equal(3, tree[typeof(CommandTest)].Count());
-        Assert.Equal(2, tree["Exception.Handle.Command.Default"].Count());
+        Assert.Equal(4, tree.Count());
+    }
+
+    [Fact]
+    public void RegisterExceptionHandlersWithSameHashCodesThrowException()
+    {
+        var mockHandler = new Mock<IHandler>();
+        var dict = new Dictionary<int, IHandler>();
+
+        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handler.Register",
+            (object[] args) => new RegisterHandlerCommand((IEnumerable<Type>)args[0], (IHandler)args[1])
+        ).Execute();
+        IoC.Resolve<ICommand>("IoC.Register", "Exception.Handler.Tree", (object[] args) => dict).Execute();
+
+        IoC.Resolve<SpaceGame.Lib.ICommand>("Exception.Handler.Register", new Type[] { typeof(CommandTest), typeof(IOException) }, mockHandler.Object).Execute();
+
+        Assert.Throws<ArgumentException>(() =>
+            IoC.Resolve<SpaceGame.Lib.ICommand>(
+                "Exception.Handler.Register", new Type[] { typeof(IOException), typeof(CommandTest) },
+                mockHandler.Object
+            ).Execute()
+        );
     }
 }
--- SpaceGame.Lib/Commands/RegisterHandlerCommand.cs
+++ SpaceGame.Lib/Commands/RegisterHandlerCommand.cs
@@ -3,20 +3,20 @@ using Hwdtech;
 
 public class RegisterHandlerCommand : ICommand
 {
-    private object _command, _exception;
+    private IEnumerable<Type> _types;
     private IHandler _handler;
 
-    public RegisterHandlerCommand(object command, object exception, IHandler handler)
+    public RegisterHandlerCommand(IEnumerable<Type> types, IHandler handler)
     {
-        _command = command;
-        _exception = exception;
+        _types = types;
         _handler = handler;
     }
 
     public void Execute()
     {
-        var tree = IoC.Resolve<IDictionary<object, IDictionary<object, IHandler>>>("Exception.Handle.Tree");
-        tree.TryAdd(_command, new Dictionary<object, IHandler>());
-        tree[_command].Add(_exception, _handler);
+        IoC.Resolve<IDictionary<int, IHandler>>("Exception.Handler.Tree").Add(
+            String.Join("_", _types.Select(x => x.GetHashCode()).OrderBy(x => x)).GetHashCode(),
+            _handler
+        );
     }
 }
