91fc268ecad3080e2c5011c9634da35851b39d5d
andreas <podovinov2003andrey@gmail.com>
2023-03-02 17:52:51 +0600

    add base console app

---
+++ SpaceGame.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SpaceGame.Console;
+using System;
+using Hwdtech;
+using Hwdtech.Ioc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting server...");
+        // IoC.Resolve<ICommand>("Server.Start", args[0]).Execute();
+
+        Console.WriteLine("Server started successfully");
+
+        Console.ReadKey();
+
+        Console.WriteLine("Stopping server...");
+
+        // IoC.Resolve<ICommand>("Server.Stop.Soft").Execute();
+
+        //* Ожидаем завершения всех потоков
+
+        Console.WriteLine("Server stopped successfully");
+    }
+}
